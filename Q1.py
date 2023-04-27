86da831af09eb985d452c6f58f637f72dcc4c2b3
ArthurPessoaIFRN <132000629+ArthurPessoaIFRN@users.noreply.github.com>
2023-04-27 14:27:08 -0300

    Update Q1.py

--- Q1.py
+++ Q1.py
@@ -1,1 +1,12 @@
+A = int(input('Bote o primeiro Numero aqui'))
+B = int(input('Bota o segundo Numero aqui'))
+def Maior(A , B):
+ if A < B:
+  return B
+ if B < A:
+  return A
+ if B == A:
+  return 'Numeros Iguais'
 
+N = Maior(A, B)
+print('maior numero é:', N)
